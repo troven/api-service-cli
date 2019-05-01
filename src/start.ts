7dabd123285b7fbdc3a9980b064df7ed60c28891
Lee Curtis <lee@apigeeks.com>
2019-05-02 01:15:28 +1000

    release v0.34.3

--- src/start.ts
+++ src/start.ts
@@ -3,7 +3,6 @@
 import { default_features } from "./index";
 import { Chassis  } from "api-service-core";
 let config = require("config");
-console.log("config: %j", config);
     
 let chassis = new Chassis(config, default_features );
 chassis.start();
